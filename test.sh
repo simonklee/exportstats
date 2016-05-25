dd2dfb4fe9d22bcbb2ef04f1bb713f9d518f0638
Simon Zimmermann <simon@insmo.com>
2016-05-25 15:25:35 +0200

    test

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#curl -i "http://localhost:6070/v1/exportstats/rate/eu.website.load.webgl.launch.cnt/eu.website.load.webgl.download.cnt?t=1h3m&format=json" || exit 1
+curl -i "http://localhost:6070/v1/exportstats/rate/eu.website.load.webgl.win7.ch.launch.cnt/eu.u.webgl.win7.ch.Playing?t=1d10m&format=json&start=1464033600000"
